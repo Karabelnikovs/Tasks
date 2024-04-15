f6c49a085fd822d2ea6da2fcf067ccbdf139ea1c
Karabelnikovs <125544102+Karabelnikovs@users.noreply.github.com>
2024-04-15 13:21:22 +0300

    names

--- routes.php
+++ routes.php
@@ -4,7 +4,7 @@ return [
   "/" => "app/controllers/task_handle/tasks_view.php",
   "/tasks" => "app/controllers/task_handle/tasks_view.php",
   "/create" => "app/controllers/task_handle/tasks_create.php",
-  "/signup-succes" => "app/controllers/user_handle/signup-success.php",
+  "/signup-succes" => "app/controllers/user_handle/signup_succes.php",
   "/logout" => "app/controllers/user_handle/logout.php",
   "/login" => "app/controllers/user_handle/login.php",
   "/signup" => "app/controllers/user_handle/signup.php"
