2f635a619a7a6f79a4b6ddc45ec475448a94ca5a
Karabelnikovs <125544102+Karabelnikovs@users.noreply.github.com>
2024-04-16 12:58:57 +0300

    signup_tailwind

--- app/views/components/head.php
+++ app/views/components/head.php
@@ -6,5 +6,7 @@
     <title><?= $page_title?></title>
     <link rel="stylesheet" href="app/views/styles/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
