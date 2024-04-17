1f279ff9a874fe49fe299d6a7145a837df3bdcbe
lmoboy <steamgoyu19@gmail.com>
2024-04-17 13:43:29 +0300

    fixed logout not being on the view page

--- app/views/tasks.view.php
+++ app/views/tasks.view.php
@@ -4,11 +4,11 @@
 
 </div>
 <?php if (isset($_SESSION["user_id"])) { ?>
-    <p><a href="logout" class="btn btn-outline-danger">Log out</a></p>
-    <p><a href="create" class="btn btn-outline-info" style="position: absolute; top: 20px; right: 20px;">Add task</a></p>
+    <a href="logout" class="btn btn-outline-danger" style="position: absolute; top: 20px; left: 20px;">Log out</a>
+    <a href="create" class="btn btn-outline-info" style="position: absolute; top: 20px; right: 20px;">Add task</a>
 
 <?php } else { ?>
-    <p><a href="login" class="btn btn-outline-success">Log in</a></p>
+    <a href="login" class="btn btn-outline-success">Log in</a>
 <?php } ?>
 
 <script defer src="public/cards.js"></script>
@@ -31,91 +31,6 @@
     <h2 style="font-size: 30px;">Darbi:</h2>
 
     <div id="container"></div>
-    <!-- <div class="table-responsive">
-            <table class="table table-bordered" id="responsive-table">
-                <thead>
-                    <tr>
-                        <th scope="col">Nosaukums</th>
-                        <th scope="col">Apraksts</th>
-                        <th scope="col">Autors</th>
-                        <th scope="col">Uztaisīts</th>
-                        <th scope="col">Deadline</th>
-                        <th scope="col">Pabeigts</th>
-                        <th scope="col">Papildus</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($tasks as $index => $task): ?>
-                        <tr>
-                            <th scope="row">
-                                <?= $task["title"] ?>
-                                </td>
-                            <td>
-                                <?= $task["DESCRIPTION"] ?>
-                            </td>
-                            <td>
-                                <?= $task["username"] ?>
-                            </td>
-
-                            <td>
-                                
-                                <?= $task["created_date"] ?>
-                            </td>
-
-                            <td>
-                                <p style="color: <?= (date("Y-m-d H:i:s", time()) >= $task["deadline_date"]) ? 'red' : 'black'; ?>"><?= $task["deadline_date"] ?></p>
-                            </td>
-                            <td>
-                                <?php if (
-                                    isset($_SESSION["user_id"])
-                                    && $task["done"] == 1
-                                    && isset($_SESSION["user_id"]) == $task["user_created"]
-                                ): ?>
-                                    <form method="POST">
-                                        <input type="hidden" name="done" value="<?= $index ?>" class="form-control">
-                                        <button type="submit" class="<?=
-                                            $_SESSION["user_id"] == $task["user_created"]
-                                            ? "btn btn-success" : "btn" ?>">Ir</button>
-                                    </form> 
-                                <?php elseif (
-                                    isset($_SESSION["user_id"])
-                                    && $task["done"] == 0
-                                    && isset($_SESSION["user_id"]) == $task["user_created"]
-                                ): ?>
-                                    <form method="POST">
-                                        <input type="hidden" name="notdone" value="<?= $index ?>" class="form-control">
-                                        <button type="submit" class="<?=
-                                            $_SESSION["user_id"] == $task["user_created"]
-                                            ? "btn btn-danger" : "btn" ?>">Nav</button>
-                                    </form>
-                                <?php else: ?>
-                                    Ieejiet lai redzēt izpildijuma datus.
-                                <?php endif; ?>
-                            </td>
-                            <td>
-                                <?php if (isset($_SESSION["user_id"])) { ?>
-                                    <form method="POST" action="delete">
-                                        <input type="hidden" name="delete" value="<?= $task["id"] ?>">
-                                        <button class="<?=
-                                            $_SESSION["user_id"] == $task["user_created"]
-                                            ? "btn btn-outline-danger" : "btn" ?>" type="submit">Izdzēst</button>
-                                    </form>
-                                    <form method="POST">
-                                        <input type="hidden" name="edit" value="<?= $task["id"] ?>">
-                                        <button class="<?=
-                                            $_SESSION["user_id"] == $task["user_created"]
-                                            ? "btn btn-outline-info" : "btn" ?>" type="submit">Rediģēt</button>
-                                    </form>
-                                <?php } else { ?>
-                                    Ieejiet lai redzēt papildus opcijas.
-                                <?php } ?>
-                            </td>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        </div> -->
 </div>
 </body>
 
