73089b87162c321d0d5c27bdba3da8b9ffb322f0
Karabelnikovs <125544102+Karabelnikovs@users.noreply.github.com>
2024-04-17 12:37:54 +0300

    form

--- app/controllers/user_handle/login.php
+++ app/controllers/user_handle/login.php
@@ -7,26 +7,26 @@ require "public/Database.php";
 $is_invalid = false;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
+
     $db = new Database($config["config"]);
-    
-    
+
+
 
     $query_string = "SELECT * FROM users WHERE email = :email";
-    $params=[":email"=>$_POST["email"]];
-    
+    $params = [":email" => $_POST["email"]];
+
     $user = $db->execute($query_string, $params)->fetchAll();
-       
-    if(!empty($user)){
+
+    if (!empty($user)) {
         $user = $user[0];
         if ($user) {
-        // pswrd: 1234Aa!1
+            // pswrd: 1234Aa!1
             if (password_verify($_POST["password"], $user["PASSWORD"])) {
-                
+
                 session_start();
-                
+
                 session_regenerate_id();
-                
+
                 $_SESSION["user_id"] = $user["id"];
                 setcookie("user_id", $_SESSION["user_id"]);
                 header("Location: tasks");
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
         $is_invalid = true;
     }
-    
+
 
 }
 
