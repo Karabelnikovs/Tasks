9a6fcc9f58ace89e0e2df659930478d1d0bc9f90
MarkussIPA22 <markusspetrovs0@gmail.com>
2024-04-17 13:08:41 +0300

    lol

--- app/views/tasks_create.view.php
+++ app/views/tasks_create.view.php
@@ -1,6 +1,7 @@
 <?php require "components/head.php" ?>
 <?php if (isset($_SESSION["user_id"])) { ?>
-    <p><a href="logout" class="btn btn-outline-danger">Log out</a></p>
+    <p style="margin-top: -10px;"><a href="logout" class="btn btn-outline-danger">Log out</a></p> <br>
+
 <?php } else { ?>
     <p><a href="login" class="btn btn-outline-success">Log in</a></p>
 <?php } ?>
@@ -8,9 +9,10 @@
 
 
 <form method="post" action="create">
-    <input name="title" type="text" placeholder="title" value='<?= isset($_POST["title"]) ? $_POST["title"] : "" ?>'><br>
-    <input name="description" type="text" placeholder="description" value='<?= isset($_POST["description"]) ? $_POST["description"] : "" ?>'><br>
-    <input name="deadline_date" type="datetime-local" value='<?= isset($_POST["deadline_date"]) ? $_POST["deadline_date"] : "" ?>'><br>
+<input name="title" type="text" placeholder="Title" value="<?= isset($_POST["title"]) ? $_POST["title"] : "" ?>" style="margin-bottom: 25px;"><br>
+<input name="description" type="text" placeholder="Description" value="<?= isset($_POST["description"]) ? $_POST["description"] : "" ?>" style="margin-bottom: 20px;"><br>
+<input name="deadline_date" type="datetime-local" value="<?= isset($_POST["deadline_date"]) ? $_POST["deadline_date"] : "" ?>" style="margin-bottom: 20px;"><br>
+
     <?php 
     if (!empty($errors)) {
         foreach ($errors as $error) {
