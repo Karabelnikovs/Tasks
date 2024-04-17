6ecd4c157787ea5017ceffeeb3066ae3d955ead6
lmoboy <steamgoyu19@gmail.com>
2024-04-17 12:37:24 +0300

    better handling of user logged in and cookies

--- app/controllers/task_handle/tasks_view.php
+++ app/controllers/task_handle/tasks_view.php
@@ -2,7 +2,9 @@
 
 session_start();
 date_default_timezone_set('Europe/Riga');
-
+if(!isset($_SESSION['user_id'])){
+    die(header('Location: /login'));
+}
 $page_title = "Tasks";
 require "app/views/tasks.view.php";
 ?>
